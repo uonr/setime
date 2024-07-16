89a78a115957038e166dcc99378d876f5a1b5bf5
uonr <dev@yuru.me>
2024-07-16 18:45:29 +0900

    Initial Commit

---
+++ setime/setimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  setimeApp.swift
+//  setime
+//
+//  Created by ななななな on 2024/07/16.
+//
+
+import SwiftUI
+
+@main
+struct setimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
